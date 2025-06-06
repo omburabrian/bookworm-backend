9a993f172922ff60a50c3c64fbd02285157f7292
Jacobwwiz <jacobwwiz@gmail.com>
2025-06-05 19:09:35 -0500

    corrected the book model

--- app/models/book.model.js
+++ app/models/book.model.js
@@ -1,29 +1,3 @@
-module.exports = (sequelize, Sequelize) => {
-  return sequelize.define("book", {
-    bookId: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    ISBN: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    publicationDate: {
-      type: Sequelize.DATE,
-    },
-    cover: {
-      type: Sequelize.STRING,
-    },
-    description: {
-      type: Sequelize.TEXT,
-    },
-  });
-};
 
 module.exports = (sequelize, Sequelize) => {
   const Book = sequelize.define("book", {
