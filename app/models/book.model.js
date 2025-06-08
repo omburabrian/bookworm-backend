f5ce261258612ae162d9db46083b7e599eaa8d7c
Jacobwwiz <jacobwwiz@gmail.com>
2025-06-08 12:44:57 -0500

    change

--- app/models/book.model.js
+++ app/models/book.model.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, Sequelize) => {
       allowNull: false,
     },
     date: {
-      type: Sequelize.DATE,
+      type: Sequelize.DATEONLY,
       allowNull: true,
     },
     cover: {
