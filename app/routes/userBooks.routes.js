fbc14737333c628c1f190a6e042e458742f7ce07
Jacobwwiz <jacobwwiz@gmail.com>
2025-06-16 01:50:40 -0500

    added auth

--- app/routes/userBooks.routes.js
+++ app/routes/userBooks.routes.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
   var router = require("express").Router();
 
     // Create a new UserBook entry
-    router.post("/userBooks/", UserBooks.add);      
+    router.post("/userBooks/",[authenticateRoute], UserBooks.add);      
     // Retrieve all books for a specific user
     router.get("/userBooks/:userId", UserBooks.findByUser);
     // Remove a book from a user's list
@@ -11,7 +11,7 @@ module.exports = (app) => {
     //check if a book is in a user's list
     router.get("/userBooks/check/:userId/:bookId", UserBooks.checkBookInUserList);
     //Update a UserBook entry
-    router.put("/userBooks/update/:userId/:bookId", UserBooks.updateUserBook);
+    router.put("/userBooks/update/:userId/:bookId",[authenticateRoute], UserBooks.updateUserBook);
 
 
 
